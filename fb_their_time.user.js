b53951a666e818dbed992a8e2b1cff76a181a2dd
Jason J <admin@ovrhere.com>
2013-09-15 15:06:18 -0400

    Culled some old code, reworked fb dom func, implemented basic Google time search.
    [Rem]: Removed the codeinjector and the injected code.
    [Add]: Added google function that looks for the "current time in " + location.
    [Mod]: Modified the fbLocationFromDOM to return the possible locations as an associative array.
    [Mod]: Modified the facebookLocation function to handle a callback onload+parsing the location.

--- fb_their_time.user.js
+++ fb_their_time.user.js
@@ -8,87 +8,6 @@
 // @version     0.1.1
 // ==/UserScript==
 
-/**
- * Used to inject pieces of userscripts into the page 
- * (to circumvent to greasemonkey's sandbox isolating userscripts from page scripts)
- * and allowing the page to excute the scripts.
- * <br>
- * Usage:
- * <ul>
- * <li>Create a <code>function functionWrapper()</code> with <code>var startTag;</code> and <code>var endTag;</code></li>
- * <li>Call <code>CodeInjector().fullInjection(functionWrapper, 'startTag', 'endTag');</code></li>
- * </ul>
- * The code between the tags will be injected into the page from the userscript and be executed from its domain.
- * 
- * @this {CodeInjector}
- * @version 0.3.1
- */
- function CodeInjector()
- {
-   var codeInjectorId = "my-injected-script";
-   return {
-     
-     /**
-      * Returns the passed script as a string.
-      * @param {function} scriptContainer The script container to turn to a string.
-      * @return {String} The function code as a string.
-      */
-     functionToString:function(scriptContainer)
-     {
-    return (new Function(scriptContainer)).toString()
-     },
-     
-      /**
-      * Uses string manipulation on the code for injection into the page. 
-      * NOTE: Must use the format 'var [start];' and 'var [end];' 
-      *   encapsulating the code to be injected.
-      * @param {String} fullScript The fullscript to trime.
-      * @param {String} start The starting tag of injected code.
-      * @param {String} end The end tag of injected code.
-      * @return {String} The code to be injected.
-      */
-      trimmer:function(fullScript, start, end)
-      { 
-          start     = 'var '+start+';';
-          end   = 'var '+end+';';
-          var index1 = fullScript.indexOf(start) + start.length;
-          var index2 = fullScript.indexOf(end);
-          return fullScript.substring(index1,index2);
-      },
-
-      /**
-      * Injects code into the page giving it the id: "my-injected-script".
-      * @param {String} inputScript to inject into page code.
-      */
-      syringe:function(inputScript)
-      {  
-          var id = codeInjectorId;
-          for (var i = 0; document.getElementById(id) ; i++) 
-            id = codeInjectorId + "-"  + i; //make the script unique 
-          var script = document.createElement('script');
-            script.setAttribute("type", "application/javascript"); 
-            script.setAttribute("id", id);
-          var scriptText = document.createTextNode(inputScript);
-          script.appendChild(scriptText);
-          document.getElementsByTagName('head')[0].appendChild(script);
-      },
-      
-      /**
-      * Parses the scriptContainer, trims it, and injects it into the page.
-      * @param {function} scriptContainer The script container to turn to a string.
-      * @param {String} start The starting tag of injected code.
-      * @param {String} end The end tag of injected code.
-      * @see CodeInjector.functionToString
-      * @see CodeInjector.trimmer
-      * @see CodeInjector.syringe
-      */
-      fullInjection:function(scriptContainer, start, end)
-      {  
-    this.syringe(  this.trimmer( this.functionToString(scriptContainer), start, end ) );
-      }
-   };
-
- }
 
  my_log = function (arg) { unsafeWindow.console.log(arg); }
 
@@ -109,108 +28,38 @@ storage = {
     }
 }
 
-function toBeInjected(){
-var BEGIN_INJECTION;
-my_log = console.log;
-myHttpRequests = {
-    
-    /** Request object. */
-    xmlhttp: new XMLHttpRequest(),
-    
-    /** The class used to contain "about me" summaries on the front page. */
-    aboutClass: '_4_ug',    
-    
-    /** Gathers the location from the DOM. 
-     * @param {Element} dom The dom the analise and check. 
-     * @return {String} The location in the "Lives in " string
-     * or, if not available, the location inf the "From " string. 
-     * If neither are available returns "". */
-    fbLocationFromDOM:function(dom){
-        var aboutPerson = dom.getElementsByClassName(this.aboutClass)
-        var loc1 = ""; //the "lives in " location
-        var loc2 = ""; //the "from" location
-        my_log("aboutPerson: " + aboutPerson + " " + aboutPerson.length);
-        var trying = dom.innerHTML;
-        my_log("check: " + dom);
-        my_log("check: " + trying);
-        my_log("check: " + trying.constains("Etienne"));
-        for (var i = aboutPerson.length - 1; i >= 0; i--){
-          var string = ""+aboutPerson[i].innerHTML;
-          if (string.contains("Lives in", 0)){
-            loc1 = aboutPerson[i].innerHTML.getElementsByTagName('a')[0].innerHTML;    
-          } else if (string.contains("From", 0)){
-              loc2 = aboutPerson[i].innerHTML.getElementsByTagName('a')[0].innerHTML;
-          }
-        };
-        return loc1 ? loc1 : ( loc2 ? loc2 : "");
-    },
-    /** 
-     * 
-     *Prints facebook location.
-     * @param {String} dest The facebook profile to GET. 
-     * 
-     * TODO Return faceook location 
-     * */
-    facebookLocation:function(dest) {
-        my_log("trying "+dest);
-        this.xmlhttp.onreadystatechange=function(){
-            var xmlHttp = myHttpRequests.xmlhttp;
-            //if complete and ok
-            if (xmlHttp.readyState == 4 && xmlHttp.status==200 ){
-                var text =   xmlHttp.responseText;
-                xmlHttp.responseXML = new DOMParser()
-                    .parseFromString(text, "text/html");
-                my_log("response: " + xmlHttp.responseXML + " " +  text);
-                my_log("location?: " + text.contains("Lives in") + " " +  text.contains("From"));
-                var dom = xmlHttp.responseXML.documentElement;
-                myHttpRequests.fbLocationFromDOM(dom);
-            }
-            if (xmlHttp.readyState == 3) //processing
-                console.log("(injected) Loading..."); 
-        }
-        this.xmlhttp.open("GET", dest, true);
-        //this.xmlhttp.send();   
-    }
-}
 
-//myHttpRequests.facebookLocation(""); 
-
-var END_INJECTION;    
-}
-
-/** Handles httpRequests. */
+/** Handles httpRequests. 
+ * @version 0.1.0 */
 myHttpRequests = {
     /** The class used to contain "about me" summaries on the front page. */
     aboutClass: '_4_ug',
+    /** The query stub to determine a location's time. */
+    googleQueryStub: 'current time in ', 
     
     /** Gathers the location from the DOM. 
      * @param {Element} dom The dom the analise and check. 
-     * @return {String} The location in the "Lives in " string
-     * or, if not available, the location inf the "From " string. 
-     * If neither are available returns "". */
+     * @return {Object} An Object of the form:
+     * { lives: "Lives in String", from: "From String"}. The default values are blank strings.
+     * */
     fbLocationFromDOM:function(dom){
-        var aboutPerson = dom.getElementsByClassName('_4_ug');//myHttpRequests.aboutClass)
-        //dom.getElementsByTagName('li')
-        var loc1 = ""; //the "lives in " location
-        var loc2 = ""; //the "from" location
-        my_log()
+        var aboutPerson = dom.getElementsByClassName('_4_ug');
+        var livesIn = ""; 
+        var fromLoc = "";
+        
         my_log("aboutPerson: " + aboutPerson + " " + aboutPerson.length);
         
-        var trying = dom.innerHTML;
-        
-        my_log("check: " + trying.contains("Etienne"));
         for (var i = aboutPerson.length - 1; i >= 0; i--){
           var string = ""+aboutPerson[i].innerHTML
           my_log("li["+i+"] :" + string);
           if (string.contains("Lives in", 0)){
-            loc1 = aboutPerson[i].getElementsByTagName('a')[0].innerHTML;
+            livesIn = aboutPerson[i].getElementsByTagName('a')[0].innerHTML;
           } else if (string.contains("From", 0)){
-              loc2 = aboutPerson[i].getElementsByTagName('a')[0].innerHTML;
+              fromLoc = aboutPerson[i].getElementsByTagName('a')[0].innerHTML;
           }
         };
-        my_log("loc1 " + loc1);
-        my_log("loc2 " + loc2);
-        return loc1 ? loc1 : ( loc2 ? loc2 : "");
+        my_log(livesIn+fromLoc)
+        return { lives: livesIn, from: fromLoc };
     },
     /** Facebook likes to pass around code with sections commented out. 
      * Let's strip those comment tags. 
@@ -229,10 +78,12 @@ myHttpRequests = {
      * 
      *Prints facebook location.
      * @param {String} dest The facebook profile to GET. 
-     * 
-     * TODO Return faceook location 
+     * @param {Function} callback (Optional.) If set, the action to perform on load. 
+     * Expects one parameter: Object/Associative array. Where the members are:
+     * lives: "Lives in String", from: "From String". 
+     *  
      * */
-    facebookLocation:function(dest) {
+    facebookLocation:function(dest, callback) {
         my_log("trying "+dest);
         GM_xmlhttpRequest({
           method: "GET",
@@ -253,31 +104,65 @@ myHttpRequests = {
                     my_log("recevied.");
                     responseXML = response.responseXML;
                 }
-                my_log("Trying "+ responseXML.documentElement);
-                my_log("Trying "+ responseXML.documentElement.getElementsByTagName('script').length);
-                my_log("Trying "+myHttpRequests.fbLocationFromDOM(responseXML.documentElement));
+                var locs = myHttpRequests.fbLocationFromDOM(responseXML.documentElement);
                 
-                
-                /*
-                my_log([
-                                  response.status,
-                                  response.statusText,
-                                  response.readyState,
-                                  response.responseHeaders,
-                                  response.responseText,
-                                  response.finalUrl,
-                                  responseXML].join("\n")); */
+                if (callback){
+                    my_log("trying function: " + callback + locs);
+                    callback(locs);
+                }
                   
             },
         
-        onprogress:function(response){
-            my_log("loading...");
-        },
-        onerror: function(response){
-            my_log("error!");
-        }
+            onprogress:function(response){
+                my_log("loading...");
+            },
+            onerror: function(response){
+                my_log("error!");
+            }
         }
         );
+    },
+    
+    /** Attempts to google the location to get the current time their.
+     * @param {String} location The location to search and check the time of.
+     * @param {Function} callback (Optional.) If set, the action to perform on load. 
+     * Will pass parameter {Object} of the form:
+     * { lives: "Lives in String", from: "From String"}, 
+     * where the default values are blank strings.
+     */
+    googleCurrentTime:function(location, callback){
+        var dest = "http://www.google.com/search?q=" +myHttpRequests.googleQueryStub + location;
+        //postStub.replace(/ /gi, "\+"); //
+        GM_xmlhttpRequest({
+          method: "GET",
+          url: dest,
+          headers: {
+            "User-Agent": "Mozilla/5.0",    // If not specified, navigator.userAgent will be used.
+            "Accept": "text/xml"            // If not specified, browser defaults will be used.
+          },
+          onload: function(response) {
+              var responseXML; 
+                if (!response.responseXML) {
+                    my_log("parsing...");                    
+                  responseXML = new DOMParser().parseFromString(response.responseText, "text/html");
+                  my_log("no error?...");                        
+                } else {
+                    my_log("recevied.");
+                    responseXML = response.responseXML;
+                }
+                
+                my_log("Google result: " + 
+                responseXML.documentElement.getElementsByClassName('vk_c vk_gy vk_sh')[0].innerHTML);
+            },
+        
+            onprogress:function(response){
+                my_log("loading...");
+            },
+            onerror: function(response){
+                my_log("error!");
+            }
+        }
+        ); 
     }
 }    
 
@@ -301,10 +186,18 @@ pageMonitor = {
     /** Whether or not currently fetching. */
     fetching: false,
     
-    /** Checks the cookies. */
+    /** Checks to see current chat's open and registers/deregisters 
+     * their 'onhover' events. */
     checkChats:function(){
         
-        var checkedSet = document.getElementsByClassName(pageMonitor.checkedClass);                          
+        var checkedSet = document.getElementsByClassName(pageMonitor.checkedClass);
+        
+        if (pageMonitor.runonce) return;
+        //var chatPaglet = document.getElementById('ChatTabsPagelet');        
+        //chatPaglet.addEventListener ("DOMNodeInserted", function(){my_log("inserted")}, false);
+        //chatPaglet.addEventListener ("DOMNodeRemoved", function(){my_log("removed")}, false); 
+        //pageMonitor.runonce = true;                        
+        //return;
         
         if (!pageMonitor.chatSet){
             pageMonitor.chatSet = checkedSet;            
@@ -314,7 +207,7 @@ pageMonitor = {
         
         for(var i=0,j=checkedSet.length; i<j; i++){
             if (!pageMonitor.fetching){
-                myHttpRequests.facebookLocation(checkedSet[i].getAttribute("href"));
+                myHttpRequests.facebookLocation(checkedSet[i].getAttribute("href"), pageMonitor.checkLocationTime);
                 pageMonitor.fetching = true;
             }      
               my_log("checkedSet name: " + checkedSet[i].innerHTML +
@@ -322,6 +215,17 @@ pageMonitor = {
             };
         
     },
+    /** The action to perform once the fb check has completed. 
+     * @param {Object} locations Expecting an object/associative array with two members; "from" and "lives". 
+     * */
+    checkLocationTime:function(locations){
+        my_log("reached checkLocationTime: " + locations);
+        my_log(locations.lives);
+        my_log(locations.from)
+        
+        myHttpRequests.googleCurrentTime(locations.lives);
+    },
+    
     /** Starts the monitor. */ 
     start:function(){
         my_log("started");
