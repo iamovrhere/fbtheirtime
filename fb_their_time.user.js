e5888677092c5987296e9159257c9ac3aac6b0dc
Jason J <admin@ovrhere.com>
2013-09-15 18:08:38 -0400

    Narrowed down the google search to the specific result table.
    [Fix]: The last commit had a flaw; getElementById does not work the same in a DOMParser object as it does in regular DOM.
    This is because of XML's expectations with id's and the like.
    [Add]: Added a register function to fire events on dom change. However, it is not used because the httpRequests fail when it is used.
    [Rea]: Added a bunch of TODO's to remove logs.

--- fb_their_time.user.js
+++ fb_their_time.user.js
@@ -9,7 +9,9 @@
 // ==/UserScript==
 
 
- my_log = function (arg) { unsafeWindow.console.log(arg); }
+ my_log = function (arg) { 
+     unsafeWindow.console.log(arg); 
+}
 
 /** Not implemented. Reads and writies objects to storage. 
  *  @version 0.1.0 */
@@ -43,15 +45,14 @@ myHttpRequests = {
      * { lives: "Lives in String", from: "From String"}. The default values are blank strings.
      * */
     fbLocationFromDOM:function(dom){
-        var aboutPerson = dom.getElementsByClassName('_4_ug');
+        var aboutPerson = dom.getElementsByClassName(myHttpRequests.aboutClass);
         var livesIn = ""; 
         var fromLoc = "";
         
-        my_log("aboutPerson: " + aboutPerson + " " + aboutPerson.length);
+        my_log("aboutPerson: " + aboutPerson + " " + aboutPerson.length); //TODO remove log
         
         for (var i = aboutPerson.length - 1; i >= 0; i--){
           var string = ""+aboutPerson[i].innerHTML
-          my_log("li["+i+"] :" + string);
           if (string.contains("Lives in", 0)){
             livesIn = aboutPerson[i].getElementsByTagName('a')[0].innerHTML;
           } else if (string.contains("From", 0)){
@@ -84,43 +85,43 @@ myHttpRequests = {
      *  
      * */
     facebookLocation:function(dest, callback) {
-        my_log("trying "+dest);
+        my_log("trying "+dest); //TODO remove log
         GM_xmlhttpRequest({
           method: "GET",
           url: dest,
           headers: {
             "User-Agent": "Mozilla/5.0",    // If not specified, navigator.userAgent will be used.
-            "Accept": "text/xml"            // If not specified, browser defaults will be used.
+            "Accept": "text/html"            // If not specified, browser defaults will be used.
           },
           onload: function(response) {
               var responseXML; 
-                if (!response.responseXML) {
-                    my_log("parsing...");
+                if (!response.responseXML) {                    
                   responseXML = new DOMParser()
                     .parseFromString( 
                         myHttpRequests.stripCommentTags(response.responseText), 
-                        "text/html");                        
-                } else {
-                    my_log("recevied.");
+                        "text/html");
+                                                
+                } else {                    
                     responseXML = response.responseXML;
                 }
-                var locs = myHttpRequests.fbLocationFromDOM(responseXML.documentElement);
                 
-                if (callback){
-                    my_log("trying function: " + callback + locs);
+                var locs = myHttpRequests.fbLocationFromDOM(responseXML.documentElement);
+                my_log("tried "+dest); //TODO remove log
+                if (callback){                    
                     callback(locs);
                 }
                   
             },
         
             onprogress:function(response){
-                my_log("loading...");
+                my_log("loading..."); //TODO remove log
             },
             onerror: function(response){
-                my_log("error!");
+                my_log("error!"); //TODO remove log
             }
         }
         );
+        my_log("GM_httpReq? "+ GM_xmlhttpRequest); //TODO remove log
     },
     
     /** Attempts to google the location to get the current time their.
@@ -138,28 +139,39 @@ myHttpRequests = {
           url: dest,
           headers: {
             "User-Agent": "Mozilla/5.0",    // If not specified, navigator.userAgent will be used.
-            "Accept": "text/xml"            // If not specified, browser defaults will be used.
+            "Accept": "text/html"            // If not specified, browser defaults will be used.
           },
           onload: function(response) {
               var responseXML; 
                 if (!response.responseXML) {
-                    my_log("parsing...");                    
-                  responseXML = new DOMParser().parseFromString(response.responseText, "text/html");
-                  my_log("no error?...");                        
+                    my_log("parsing...");     //TODO Remove log
+                    responseXML = new DOMParser().parseFromString(response.responseText, "text/html"); 
                 } else {
-                    my_log("recevied.");
+                    my_log("recevied."); //TODO Remove log
                     responseXML = response.responseXML;
                 }
+                var answer = responseXML.documentElement.getElementsByClassName('obcontainer');
+                //responseXML.documentElement.getElementsByClassName('vk_c vk_gy vk_sh');
+                if (answer.length <= 0 ){
+                  my_log("none found ");   //TODO Remove log
+                  return;  
+                } 
+                my_log("answer? ");   //TODO Remove log
+                var table = answer[0].getElementsByTagName('table');
+                my_log("answer " + table.length)  //TODO Remove log
                 
-                my_log("Google result: " + 
-                responseXML.documentElement.getElementsByClassName('vk_c vk_gy vk_sh')[0].innerHTML);
+                for(var i=0,j=table.length; i<j; i++){
+                    //if (table[i].getElementsByTagName[td].length == 1)
+                        my_log("table "+i+" : "+table[i].innerHTML); //TODO Remove log
+                };
+                //my_log("Google result: " + answer[0].innerHTML); //TODO Remove log
             },
         
             onprogress:function(response){
-                my_log("loading...");
+                my_log("loading..."); //TODO Remove log
             },
             onerror: function(response){
-                my_log("error!");
+                my_log("error!"); //TODO Remove log
             }
         }
         ); 
@@ -179,57 +191,102 @@ pageMonitor = {
     checkFrequency: 5000,
     /** Used for canceling the checking. */
     checkInterval: 0,
+    
     /** The current chat set. */
     chatSet: 0,
+    /** The chat pagelet container. */
+    chatPagelet: 0, 
+    
     /**The class that is checked in #checkChats().  */
     checkedClass: 'titlebarText',
+    /** The pagelet's id to monitor. */
+    chatPageletId: 'ChatTabsPagelet',
+    
     /** Whether or not currently fetching. */
     fetching: false,
+    /** Whether or not we are currently checking the chats. */
+    isCheckingChats: false,
+    
+    /** Check every 500 milliseconds to register the paglet.
+     * Once registered, we set event listeners to fire onDOMNodeInserted and onDOMNodeRemoved to  
+     * call #pageMonitor.checkChats */
+    registerPagelet:function(){
+        my_log("registering..."); //TODO Remove log
+        if (!pageMonitor.chatPaglet){
+            var e = document.getElementById(pageMonitor.chatPageletId);
+            if ( e ){
+                pageMonitor.chatPaglet = e;        
+                e.addEventListener ("DOMNodeInserted", function(){ pageMonitor.checkChats();}, false);
+                e.addEventListener ("DOMNodeRemoved", function(){ pageMonitor.checkChats(); }, false);
+                return; 
+            }
+        } 
+        setTimeout(pageMonitor.registerPagelet, 500);
+    },
     
     /** Checks to see current chat's open and registers/deregisters 
      * their 'onhover' events. */
     checkChats:function(){
+        if(pageMonitor.isCheckingChats) return; 
+        pageMonitor.isCheckingChats = true; //this prevents multiple calls running over each other
+        my_log("checking chats..."); //TODO Remove log
         
         var checkedSet = document.getElementsByClassName(pageMonitor.checkedClass);
         
-        if (pageMonitor.runonce) return;
-        //var chatPaglet = document.getElementById('ChatTabsPagelet');        
-        //chatPaglet.addEventListener ("DOMNodeInserted", function(){my_log("inserted")}, false);
-        //chatPaglet.addEventListener ("DOMNodeRemoved", function(){my_log("removed")}, false); 
-        //pageMonitor.runonce = true;                        
-        //return;
-        
-        if (!pageMonitor.chatSet){
-            pageMonitor.chatSet = checkedSet;            
-        } else if (pageMonitor.chatSet != checkedSet){
-            pageMonitor.chatSet = checkedSet;            
+        if (!pageMonitor.chatSet){ //on first run
+            my_log("setting")  //TODO Remove log
+            //we slice to get a copy/clone rather than a reference
+            pageMonitor.chatSet = [].slice.call(checkedSet);             
+        } else {
+            var changed = checkedSet.length != pageMonitor.chatSet.length ? true : false;
+            for(var i=0,j=checkedSet.length; i<j; i++){               
+              if ( checkedSet[i] != pageMonitor.chatSet[i] ){
+                  my_log("different item at "+ i); //TODO Remove log
+                  changed=true;
+              }
+            };
+            //if changed, we recopy and continue
+            if (changed){
+                pageMonitor.chatSet = [].slice.call(checkedSet);
+                my_log("new set!")            //TODO Remove log
+            } else { //nothing has changed.
+                pageMonitor.isCheckingChats = false;
+                return;
+            }
         }
         
+        pageMonitor.isCheckingChats = false;        
+        
+        //changes happened so we do things
         for(var i=0,j=checkedSet.length; i<j; i++){
-            if (!pageMonitor.fetching){
-                myHttpRequests.facebookLocation(checkedSet[i].getAttribute("href"), pageMonitor.checkLocationTime);
-                pageMonitor.fetching = true;
-            }      
-              my_log("checkedSet name: " + checkedSet[i].innerHTML +
-                      "url: " + checkedSet[i].getAttribute("href")  );
-            };
+            myHttpRequests.facebookLocation(checkedSet[i].getAttribute("href"), pageMonitor.checkLocationTime);
+        
+            my_log("checkedSet name: " + checkedSet[i].innerHTML +
+                  "url: " + checkedSet[i].getAttribute("href")  ); //TODO Remove log
+        };
         
     },
     /** The action to perform once the fb check has completed. 
      * @param {Object} locations Expecting an object/associative array with two members; "from" and "lives". 
      * */
     checkLocationTime:function(locations){
-        my_log("reached checkLocationTime: " + locations);
-        my_log(locations.lives);
-        my_log(locations.from)
-        
-        myHttpRequests.googleCurrentTime(locations.lives);
+        my_log("reached checkLocationTime: " + locations); //TODO Remove log
+        my_log(locations.lives); //TODO Remove log
+        my_log(locations.from); //TODO Remove log
+        if (locations.lives){
+            myHttpRequests.googleCurrentTime(locations.lives);
+        } else if (locations.from) {
+            myHttpRequests.googleCurrentTime(locations.from);
+        } else {
+            my_log("nothing available. =/") //TODO Remove log
+        }
     },
     
     /** Starts the monitor. */ 
     start:function(){
-        my_log("started");
-        this.checkInterval = setInterval(this.checkChats, this.checkFrequency, null);
+        my_log("started"); //TODO Remove log
+        //pageMonitor.registerPagelet();
+        pageMonitor.checkInterval = setInterval(this.checkChats, this.checkFrequency, null);
     },
     /** Stops the monitor. */
     stop:function(){
@@ -239,4 +296,4 @@ pageMonitor = {
 
 //start monitoring
 pageMonitor.start();
-CodeInjector().fullInjection(toBeInjected, "BEGIN_INJECTION", "END_INJECTION")
+
